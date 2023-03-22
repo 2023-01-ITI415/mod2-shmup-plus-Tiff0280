8a83d8c3ef02a261299636d10acca861f7948853
Tiff0280 <ty194@scarletmail.rutgers.edu>
2023-03-21 21:40:08 -0400

    Added New Weapon

--- Assets/__Scripts/Weapon.cs
+++ Assets/__Scripts/Weapon.cs
@@ -150,6 +150,11 @@ public class Weapon : MonoBehaviour
                 p.transform.rotation = Quaternion.AngleAxis(-15, Vector3.back);
                 p.vel = p.transform.rotation * vel;
                 break;
+
+            case eWeaponType.laser:
+                p = MakeProjectile();
+                p.vel = vel;
+                break;
         }
     }
 
